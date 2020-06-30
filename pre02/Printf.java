51845f35171ff1c97e2ede7ecd8992cdf9b56944
Michael Alfano <malfano3@schools.nyc.gov>
2020-06-30 16:37:48 -0400

    Added Printf.java file

---
+++ pre02/Printf.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Printf {
+
+  public static void main(String[] args) {
+    int x = 5;
+    double y = 2.14;
+
+    // correct statement
+    System.out.printf("My value = %d%n", x );
+
+    // incorrect statement
+    // Throws illegal format conversion exception
+    //System.out.printf("My value = %f%n", x );
+
+    // incorrect statement
+    // Throws illegal format conversion exception
+    System.out.printf("My value = %d%n", y );
+
+  } // end public static void main
+} // end class Printf
