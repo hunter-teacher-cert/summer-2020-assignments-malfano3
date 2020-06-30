5b5a0b73dd5f0a804ff5e80c0f1d0d403069b77c
Michael Alfano <malfano3@schools.nyc.gov>
2020-06-30 14:09:08 -0400

    Added Date.java

---
+++ pre02/Date.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+
+  public static void main(String[] args) {
+    String day = "Tuesday";
+    String date = "30";
+    String month = "June";
+    String year = "2020";
+
+    System.out.println("American Format:");
+    System.out.println(day + ", " + month + " " + date + ", " + year);
+
+    System.out.println("European Format:");
+    System.out.println(day + " " + date + " " + month + " " + year);
+
+  } // end public static void main
+} // end class Hello
