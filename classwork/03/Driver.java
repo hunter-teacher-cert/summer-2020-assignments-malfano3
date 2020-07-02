4630b4bddac5a92043d3b9fdad6f0c18cb3b7bc6
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-02 13:11:47 -0400

    added Driver.java

---
+++ classwork/03/Driver.java
@@ -0,0 +1,9 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver {
+    public static void main(String[] args) {
+	     Time t = new Time(8, 15, 22);
+       Time.printTime(t);
+    } // end main
+} // end class Driver
