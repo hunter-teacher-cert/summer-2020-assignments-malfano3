cd641c3a97a58d52975547bf17f73232ba8cad58
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-08 14:17:52 -0400

    updated

---
+++ classwork/05/Driver.java
@@ -0,0 +1,61 @@
+import java.io.*;
+
+import java.util.*;
+
+public class Driver{
+  public static void main(String[] args) {
+
+
+    LList l = new LList();
+
+
+    l.add("Coitenly");
+    System.out.println(l);
+    l.add("Curly");
+    System.out.println(l);
+    l.add("Larry");
+    System.out.println(l);
+
+    l.add("Harpo");
+    l.add("Groucho");
+    l.add("Chico");
+    l.add("Zeppo");
+    l.add("Gummo");
+    System.out.println(l);
+
+
+
+
+
+
+
+
+
+
+
+
+    // Node n1;
+    // Node n2;
+
+    // n1 = new Node("Tim");
+    // n2 = new Node("Arthur");
+    // n1.setNext(n2);
+    // System.out.println(n1);
+    // System.out.println(n2);
+    // System.out.println(n1.getNext());
+
+    // n1.getNext().setData("Robin");
+    // System.out.println(n1);
+    // System.out.println(n2);
+
+    // Node n3 = new Node("Merlin",n2);
+    // n1.setNext(n3);
+    // System.out.println(n1);
+    // System.out.println(n1.getNext());
+    // System.out.println(n1.getNext().getNext());
+
+
+
+
+  }
+}
