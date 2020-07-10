713ee7fcd20a8fb3db089cbae2cf730bed4e244b
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-10 15:39:54 -0400

    Added files

--- classwork/04/SuperArray.java
+++ classwork/04/SuperArray.java
@@ -3,23 +3,23 @@ import java.util.*;
 
 
 public class SuperArray {
-  private int[] data;
-  private int numberElements;
+private int[] data;
+private int numberElements;
 
-  public SuperArray() {
-    this.data = new int[10];
-    this.numberElements = 0;
-  }
+public SuperArray() {
+        this.data = new int[10];
+        this.numberElements = 0;
+}
 
-  public SuperArray(int arraySize) {
-    this.data = new int[arraySize];
-    this.numberElements = 0;
-  }
+public SuperArray(int arraySize) {
+        this.data = new int[arraySize];
+        this.numberElements = 0;
+}
 
 /*
 
- // old add method (working)
-  public void add(int value) {
+   // old add method (working)
+   public void add(int value) {
     if(this.numberElements < this.data.length) {
       this.data[numberElements] = value;
       this.numberElements++;
@@ -33,49 +33,47 @@ public class SuperArray {
       this.data[numberElements] = value;
       this.numberElements++;
     }
-  }
+   }
 
-*/
-
-
-  public void add(int value) {
-    this.add(this.numberElements, value);
-  }
+ */
 
 
+public void add(int value) {
+        this.add(this.numberElements, value);
+}
 
 
 
-
-
-
-
-  public void add(int index, int value) {
-    if(index < this.numberElements) { // index filled, shift required
-      for(int i = numberElements; i > index; i--) {
-        this.data[i] = this.data[i - 1];
-      }
-      this.data[index] = value;
-      this.numberElements++;
-    }
-    else if(index >= this.numberElements && index < this.data.length) { // index free and in current array
-        this.data[index] = value;
-        this.numberElements++;
-    }
-    else { // index out of bounds
-      boolean indexValid = false;
-      while(indexValid == false){
-        this.grow();
-        indexValid = index < this.data.length;
-      }
-      this.data[index] = value;
-      this.numberElements++;
-    }
-  }
+public int getNumberElements() {
+        return this.numberElements;
+}
 
 
 
 
+
+public void add(int index, int value) {
+        if(index < this.numberElements) { // index filled, shift required
+                for(int i = numberElements; i > index; i--) {
+                        this.data[i] = this.data[i - 1];
+                }
+                this.data[index] = value;
+                this.numberElements++;
+        }
+        else if(index >= this.numberElements && index < this.data.length) { // index free and in current array
+                this.data[index] = value;
+                this.numberElements++;
+        }
+        else { // index out of bounds
+                boolean indexValid = false;
+                while(indexValid == false) {
+                        this.grow();
+                        indexValid = index < this.data.length;
+                }
+                this.data[index] = value;
+                this.numberElements++;
+        }
+}
 
 
 
@@ -94,15 +92,19 @@ public class SuperArray {
 
 
 
-  public void remove(int index) {
-    if (index <= numberElements - 1 && index >= 0) {
-      for(int i = index; i < numberElements - 1; i++) {
-        this.data[i] = this.data[i+1];
-      }
-      this.data[numberElements] = 0;
-      numberElements--;
-    }
-  }
+
+
+
+
+public void remove(int index) {
+        if (index <= numberElements - 1 && index >= 0) {
+                for(int i = index; i < numberElements - 1; i++) {
+                        this.data[i] = this.data[i+1];
+                }
+                this.data[numberElements] = 0;
+                numberElements--;
+        }
+}
 
 
 
@@ -113,51 +115,51 @@ public class SuperArray {
 
 
 
-  public int get(int index) {
-    if(index < this.data.length) {
-      return this.data[index];
-    }
-    else {
-      return -1;
-    }
-  }
+public int get(int index) {
+        if(index < this.data.length) {
+                return this.data[index];
+        }
+        else {
+                return -1;
+        }
+}
 
-  public boolean isEmpty() {
-    if(this.numberElements == 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
-  }
-
-
-  public String toString() {
-    String myString = new String();
-
-    for(int i = 0; i < numberElements; i++) {
-      myString += this.data[i] + " ";
-    } // end for loop
-
-    return myString;
-  }
+public boolean isEmpty() {
+        if(this.numberElements == 0) {
+                return true;
+        }
+        else {
+                return false;
+        }
+}
 
 
-  public void grow() {
-    int[] newArray = new int[this.data.length + 10];
-    for(int i = 0; i < this.data.length; i++) {
-      newArray[i] = this.data[i];
-    }
-    this.data = newArray;
-  }
+public String toString() {
+        String myString = new String();
+
+        for(int i = 0; i < numberElements; i++) {
+                myString += this.data[i] + " ";
+        } // end for loop
+
+        return myString;
+}
 
 
+public void grow() {
+        int[] newArray = new int[this.data.length + 10];
+        for(int i = 0; i < this.data.length; i++) {
+                newArray[i] = this.data[i];
+        }
+        this.data = newArray;
+}
 
 
 
 
 
-  public static void main(String[] args) {
+
+
+public static void main(String[] args) {
 
 /*
 
@@ -192,9 +194,9 @@ public class SuperArray {
     System.out.println(myArray.toString());
     System.out.println();
     myArray.grow();
-*/
+ */
 
-  } // end main method
+}   // end main method
 
 
 
---
+++ classwork/08/MyStack.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class MyStack {
+        // add your internal data structure here
+        private SuperArray a;
+        private int numItems;
+
+public MyStack(){
+        // add constructor code
+        this.a = new SuperArray(0);
+        this.numItems = this.a.getNumberElements();
+}
+
+public void push(String data){
+        // add code to push data on to the stack
+}
+
+public String pop(){
+        // add code to remove and return the
+        // item on the top of the stack
+}
+
+public String top(){
+        // add code to return but not remove the
+        // item on the top of the stack
+}
+
+public boolean isEmpty(){
+        // return true if the stack has no data, false otherwise
+}
+
+public int size(){
+        // add code to return the number of items currently on the stack
+}
+
+}
