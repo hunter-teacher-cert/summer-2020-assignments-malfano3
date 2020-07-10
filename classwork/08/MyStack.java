53b226cf2422a6990b763ed7a95ffcd8723895ea
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-10 17:16:51 -0400

    Added top, isEmpty, push

--- classwork/08/MyStack.java
+++ classwork/08/MyStack.java
@@ -14,23 +14,33 @@ public MyStack(){
 
 public void push(String data){
         // add code to push data on to the stack
-
+        this.linked_list.addFront(data);
 }
 /*
    public String pop(){
         // add code to remove and return the
         // item on the top of the stack
    }
+ */
 
-   public String top(){
+public String top(){
         // add code to return but not remove the
         // item on the top of the stack
-   }
+
+        if(this.linked_list.head.getData() == null) {
+                return "Stack empty."
+        }
+        else {
+                return this.linked_list.head.getData();
+        }
+}
+
 
    public boolean isEmpty(){
         // return true if the stack has no data, false otherwise
+        this.linked_list.head.getData() == null;
    }
-
+/*
    public int size(){
         // add code to return the number of items currently on the stack
    }
--- classwork/08/MyStackDriver.java
+++ classwork/08/MyStackDriver.java
@@ -3,4 +3,13 @@ import java.util.*;
 
 public class MyStackDriver {
 
-}
+public static void main(String[] args) {
+
+        MyStack s = new MyStack();
+        s.push("TEST");
+        System.out.println(s);
+
+} // end main
+
+
+} // end MyStackDriver
