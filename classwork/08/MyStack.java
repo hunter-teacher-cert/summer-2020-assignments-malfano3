e9ce36b8f0a1295377cc54d0c71b14e2c3c3b3cb
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-10 23:34:04 -0400

    fixed pop method

--- classwork/08/MyStack.java
+++ classwork/08/MyStack.java
@@ -9,40 +9,64 @@ private int numItems;
 public MyStack(){
         // add constructor code
         this.linked_list = new LList();
-        this.numItems = this.linked_list.numNodes();
+        this.numItems = this.linked_list.getNumNodes();
 }
 
 public void push(String data){
         // add code to push data on to the stack
         this.linked_list.addFront(data);
 }
-/*
-   public String pop(){
+
+public String pop(){
         // add code to remove and return the
         // item on the top of the stack
-   }
- */
+        String result = this.linked_list.getHead().getData();
+        this.linked_list.setHeadNext();
+        this.linked_list.reduceNumNodes();
+        return result;
+}
+
 
 public String top(){
         // add code to return but not remove the
         // item on the top of the stack
 
-        if(this.linked_list.head.getData() == null) {
-                return "Stack empty."
+        if(this.linked_list.getHead().getData() == null) {
+                return "Stack empty.";
         }
         else {
-                return this.linked_list.head.getData();
+                return this.linked_list.getHead().getData();
         }
 }
 
 
-   public boolean isEmpty(){
+public boolean isEmpty(){
         // return true if the stack has no data, false otherwise
-        this.linked_list.head.getData() == null;
-   }
-/*
-   public int size(){
+        return this.linked_list.getHead().getData() == null;
+}
+
+public int size(){
         // add code to return the number of items currently on the stack
-   }
- */
+        return this.numItems;
+}
+
+public String toString(){
+        String s = "< ";
+
+        Node tmp;
+        tmp = this.linked_list.getHead();
+        while (tmp != null) {
+                s = s + tmp.getData()+" ";
+                // how can we now move tmp to the next node
+                // this is the magic linked list idiom!!!!
+                // this moves a pointer to the next node!!!
+                // It's analagous to i=i+1 for an array (for loop)
+                tmp = tmp.getNext();
+        }
+
+
+        s = s + ">";
+        return s;
+}
+
 } // class MyStack
