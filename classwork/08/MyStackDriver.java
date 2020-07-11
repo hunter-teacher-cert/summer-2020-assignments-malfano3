9f583bb20039ebcf147189b8e78cf14324bf38bf
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-10 23:35:06 -0400

    corrected Driver file to incorporate new pop method

--- classwork/08/MyStackDriver.java
+++ classwork/08/MyStackDriver.java
@@ -7,7 +7,22 @@ public static void main(String[] args) {
 
         MyStack s = new MyStack();
         s.push("TEST");
+        s.push("ANOTHER");
+        s.push("NODE");
         System.out.println(s);
+              // push seems to be working
+              // Expected output: < NODE ANOTHER TEST >
+
+
+        s.pop();
+        System.out.println(s); // Expected output: < ANOTHER TEST >
+        s.pop();
+        System.out.println(s); // Expected output: < TEST >
+        s.pop();
+        System.out.println(s); // Expected output: < >
+              // pop seems to be working
+
+
 
 } // end main
 
