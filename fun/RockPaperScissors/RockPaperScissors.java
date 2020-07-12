048f77782fe468bca467156972ab4a09f3a39945
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-12 11:44:32 -0400

    new file, new directory

--- fun/RockPaperScissors/RockPaperScissors.java
+++ fun/RockPaperScissors/RockPaperScissors.java
@@ -3,11 +3,13 @@ import java.io.*;
 
 public class RockPaperScissors {
 
-
+Scanner myScanner = new Scanner(System.in);
+System.out.println("Choose Rock, Paper, or Scissors");
+String userChoice = myScanner.nextLine();
 
 
 public static void main(String[] args) {
 
-} // end main method
+} // end main methodt
 
 } // end class RockPaperScissors
