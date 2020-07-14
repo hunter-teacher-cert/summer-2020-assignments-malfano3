bcd8ec0894cd41705815b24114e28d724db4ac68
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-14 14:19:20 -0400

    completed bianry search

--- classwork/10/Boogle.java
+++ classwork/10/Boogle.java
@@ -19,9 +19,9 @@ import java.util.*;
 public class Boogle {
 
 //return index of first occurence of target, or -1 if not found
-public static int linSearch( ArrayList al, int target )
+public static int linSearch(ArrayList al, int target)
 {
-        for( int pos=0; pos<al.size(); pos++) {
+        for(int pos = 0; pos < al.size(); pos++) {
                 if (al.get(pos).equals(target))
                         return pos;
         }
@@ -36,12 +36,29 @@ public static int linSearch( ArrayList al, int target )
 
 
 //return index of target, or -1 if not found
-public static int binSearch( ArrayList al, int target ) {
+public static int binSearch(ArrayList arr, int target) {
 
-        
+        int arrLength = arr.size();
+        int startIndex = 0;
+        int midIndex;
+        int endIndex = arrLength - 1;
 
+        while(startIndex <= endIndex) {
 
+                midIndex = (startIndex + endIndex) / 2;
 
+                if((int)arr.get(midIndex) == target) {
+                        return midIndex;
+                }
+                else if(target < (int)arr.get(midIndex)) {
+                        endIndex = midIndex - 1;
+                }
+                else if(target > (int)arr.get(midIndex)) {
+                        startIndex = midIndex + 1;
+                }
+        }
+
+        return -1;
 }  //end binSearch()
 
 
@@ -66,7 +83,7 @@ public static ArrayList prestoArrayListo(int numItems, int lo, int hi) {
                 //retArr.add( Math.random() ); // [0,1)
                 //retArr.add( (int)Math.random() ); // 0
                 //retArr.add( (int)(hi * Math.random()) ); // [0,hi)
-                retArr.add( lo + (int)(hi * Math.random()) ); // [lo,lo+hi)
+                retArr.add(lo + (int)(hi * Math.random())); // [lo,lo+hi)
         }
         return retArr;
 }  //end prestoArrayListo()
@@ -77,11 +94,11 @@ public static ArrayList prestoSortedArrayListo(int numItems, int lo, int hi) {
         ArrayList retArr = new ArrayList();
 
         //if requested, return empty ArrayList
-        if ( numItems<1 ) {
+        if (numItems<1) {
                 return retArr;
         }
         //populate index 0
-        retArr.add( (int)(lo * Math.random()) );
+        retArr.add((int)(lo * Math.random()));
 
         //populate rest of indices, each one greater than last
         for(int i=1; i<numItems; i++) {
@@ -91,15 +108,15 @@ public static ArrayList prestoSortedArrayListo(int numItems, int lo, int hi) {
                 //retArr.add( (int)(hi * Math.random()) ); // [0,hi)
                 //retArr.add( lo + (int)(hi * Math.random()) ); // [lo,lo+hi)
                 //System.out.println(retArr.get(i-1));
-                retArr.add( (int)retArr.get(i-1)
-                            + lo + (int)(hi * Math.random()) ); // ["left neighbour",hi)
+                retArr.add((int)retArr.get(i-1)
+                           + lo + (int)(hi * Math.random()));  // ["left neighbour",hi)
         }
 
         return retArr;
 }  //end prestoSortedArrayListo()
 
 
-public static void main( String[] args ) {
+public static void main(String[] args) {
         /*~~~~v~~~~~~move~me~down~~~1~block~at~a~time~~~~~~~~~~v~~~~
            System.out.println("\n"+"al000"+":");
            ArrayList al000 = prestoArrayListo(0,0,0);
@@ -121,42 +138,49 @@ public static void main( String[] args ) {
 
 
         // test battery using sorted ArrayLists as search space
-        /*~~~~v~~~~~~move~me~down~~~1~block~at~a~time~~~~~~~~~~v~~~~
            System.out.println("\n"+"sal000"+":");
            ArrayList sal000 = prestoSortedArrayListo(0,0,0);
            System.out.println(sal000);
            System.out.println(linSearch(sal000,3));
            System.out.println(binSearch(sal000,3));
+
            System.out.println("\n"+"sal00"+":");
            ArrayList sal00 = prestoSortedArrayListo(5,0,100);
            System.out.println(sal00);
            System.out.println(linSearch(sal00,3));
            System.out.println(binSearch(sal00,3));
+
            System.out.println("\n"+"sal01"+":");
            ArrayList sal01 = prestoSortedArrayListo(5,0,100);
            System.out.println(sal01);
            System.out.println(linSearch(sal01,3));
            System.out.println(binSearch(sal01,3));
+
            System.out.println("\n"+"sal02"+":");
            ArrayList sal02 = prestoSortedArrayListo(5,3,100);
            System.out.println(sal02);
            System.out.println(linSearch(sal02,3));
            System.out.println(binSearch(sal02,3));
+
            System.out.println("\n"+"sal03"+":");
            ArrayList sal03 = prestoSortedArrayListo(5,0,100);
            System.out.println(sal03);
            System.out.println(linSearch(sal03,3));
            System.out.println(binSearch(sal03,3));
+
            System.out.println("\n"+"sal04"+":");
            ArrayList sal04 = prestoSortedArrayListo(20,0,3);
            System.out.println(sal04);
            System.out.println(linSearch(sal04,3));
            System.out.println(binSearch(sal04,3));
+
            System.out.println("\n"+"sal05"+":");
            ArrayList sal05 = prestoSortedArrayListo(20,1,5);
            System.out.println(sal05);
            System.out.println(linSearch(sal05,3));
            System.out.println(binSearch(sal05,3));
+           /*~~~~v~~~~~~move~me~down~~~1~block~at~a~time~~~~~~~~~~v~~~~
+
            ~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~*/
 
         //batch of commands for easier test case generation:
