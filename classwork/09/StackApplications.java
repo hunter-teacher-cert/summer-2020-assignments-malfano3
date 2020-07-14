4d8c9b2d29e32a6732ab818f47c4a5e93362f73d
Michael Alfano <malfano3@schools.nyc.gov>
2020-07-13 20:21:59 -0400

    reverse, reverseWords, isPalindrome, parenCheck completed and working

--- classwork/09/StackApplications.java
+++ classwork/09/StackApplications.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class StackApplications {
 
-// Stack<String> myStack = new Stack<String>();
 
 public static String reverse(String s) {
         String result = new String();
@@ -13,9 +12,40 @@ public static String reverse(String s) {
                 myStack.push(Character.toString(s.charAt(i)));
         }
 
-        for(int i = 0; i < s.length(); i++) {
+        while(!myStack.empty()) {
                 result += myStack.pop();
+        } // while stack is not empty
+
+        //
+        return result;
+} // end reverse()
+
+public static String reverseWords(String s) {
+        int numWords = 0;
+        int spaceCount = 0;
+        String result = new String();
+        Stack<String> myStack = new Stack<String>();
+
+        // count how many spaces in parameter string
+        for(int i = 0; i < s.length(); i++) {
+                if(Character.toString(s.charAt(i)).equals(" ")) {
+                        spaceCount++;
+                }
         }
+        numWords = spaceCount + 1;
+
+        String[] arr = new String[numWords];
+
+        arr = s.split(" ");
+
+        for(int i = 0; i != numWords; i++) {
+                myStack.push(arr[i]);
+        }
+
+        while(!myStack.empty()) {
+                result += myStack.pop() + " ";
+        } // while stack is not empty
+
         //
         return result;
 } // end reverse()
@@ -35,30 +65,46 @@ public static boolean parenCheck(String s) {
 
         // add all characters to myStack
         for(int i = 0; i < s.length(); i++) {
-                myStack.push(Character.toString(s.charAt(i)));
-                if(Character.toString(s.charAt(i)).equals("(")) {
-                        openParenCount++;
+                if(i == 0 && Character.toString(s.charAt(i)).equals("(")) {
+                        myStack.push(Character.toString(s.charAt(i)));
+                }
+                else if(i == 0 && Character.toString(s.charAt(i)).equals(")")) {
+                        return false;
+                }
+                else if(Character.toString(s.charAt(i)).equals("(")) {
+                        myStack.push(Character.toString(s.charAt(i)));
                 }
                 else if(Character.toString(s.charAt(i)).equals(")")) {
-                        closedParenCount++;
+                        try {
+                                myStack.pop();
+                        }
+                        catch(EmptyStackException e) {
+                                return false;
+                        }
+                }
+                else {
+                        continue;
                 }
         } // end for loop
-        if(openParenCount != closedParenCount) {
-          return false;
-        }
 
-        return openParenCount == closedParenCount;
+        return myStack.empty();
+
+
 } // end parenCheck()
 
 public static void main(String[] args) {
 
+        System.out.println();
+
         System.out.println("reverse(\"esrever gnitset\"): " + reverse("esrever gnitset")); // testing reverse
-
         System.out.println("isPalindrome(\"racecar\"): " + isPalindrome("racecar")); // testing isPalindrome("racecar")
-
         System.out.println("isPalindrome(\"racecars\"): " + isPalindrome("racecars")); // testing isPalindrome("racecars")
+        System.out.println("parenCheck(\"(2+3+4(7+3) + (8-5))\"): " + parenCheck("(2+3+4(7+3) + (8-5))")); // true
+        System.out.println("parenCheck(\"(2+3+4(7+3 + (8-5))\"): " + parenCheck("(2+3+4(7+3 + (8-5))")); // false
+        System.out.println("reverseWords(\"fun is this\"): " + reverseWords("fun is this"));
 
-        System.out.println("parenCheck(\"(1+2+3) + (4*5)\"): " + parenCheck("(1+2+3) + (4*5)"));
+        System.out.println();
+
 }
 
 } // end class StackApplications
